8520c8f65fdc1e4f32948d20089a4bcda093d415
Youhan Wu <youhan.wu@outlook.com>
2020-06-06 16:25:07 +0800

    convert SettingItem to ObservableObject

--- NeuToDo/Models/SettingsModels/SettingItem.cs
+++ NeuToDo/Models/SettingsModels/SettingItem.cs
@@ -1,12 +1,55 @@
-﻿namespace NeuToDo.Models.SettingsModels
+﻿using GalaSoft.MvvmLight;
+
+namespace NeuToDo.Models.SettingsModels
 {
-    public class SettingItem
+    public class SettingItem : ObservableObject
     {
-        public string Name { get; set; }
-        public string Detail { get; set; }
-        public string Button1Text { get; set; }
-        public string Button2Text { get; set; }
-        public bool Button1Status { get; set; }
-        public bool Button2Status { get; set; }
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set => Set(nameof(Name), ref _name, value);
+        }
+
+        private string _detail;
+
+        public string Detail
+        {
+            get => _detail;
+            set => Set(nameof(Detail), ref _detail, value);
+        }
+
+        private string _button1Text;
+
+        public string Button1Text
+        {
+            get => _button1Text;
+            set => Set(nameof(Button1Text), ref _button1Text, value);
+        }
+
+        private string _button2Text;
+
+        public string Button2Text
+        {
+            get => _button2Text;
+            set => Set(nameof(Button2Text), ref _button2Text, value);
+        }
+
+        private bool _button1Status;
+
+        public bool Button1Status
+        {
+            get => _button1Status;
+            set => Set(nameof(Button1Status), ref _button1Status, value);
+        }
+
+        private bool _button2Status;
+
+        public bool Button2Status
+        {
+            get => _button2Status;
+            set => Set(nameof(Button2Status), ref _button2Status, value);
+        }
     }
 }
