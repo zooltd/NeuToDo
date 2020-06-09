116bf433e1adb7c6f9ab4b545dfd3673a6e162b5
Youhan Wu <youhan.wu@outlook.com>
2020-06-09 17:05:52 +0800

    add a scratch

--- NeuToDo/Models/EventModel.cs
+++ NeuToDo/Models/EventModel.cs
@@ -1,64 +1,8 @@
 ﻿using System;
-using System.Threading.Tasks;
-using NeuToDo.Models.SettingsModels;
 using SQLite;
 
 namespace NeuToDo.Models {
-    public class NewEventModel {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-    }
 
-    public class NewNeuEventModel : NewEventModel {
-    }
-
-    public interface INewEventModelStorage<T> where T : NewEventModel {
-        Task InsertAsync(T t);
-    }
-
-    public class NewEventModelStorage<T> : INewEventModelStorage<T>
-        where T : NewEventModel {
-        private SQLiteAsyncConnection _connection;
-
-        public async Task InsertAsync(T t) {
-            await _connection.InsertAsync(t);
-        }
-    }
-
-    public interface IEventStorageProvider {
-        INewEventModelStorage<NewNeuEventModel> GetNeuEventModelStorage();
-    }
-
-    public class EventStorageProvider : IEventStorageProvider {
-        public INewEventModelStorage<NewNeuEventModel> GetNeuEventModelStorage() {
-           return new NewEventModelStorage<NewNeuEventModel>();
-        }
-    }
-
-    public class NeuLogin {
-
-        private INewEventModelStorage<NewNeuEventModel> _eventModelStorage;
-
-        public NeuLogin(IEventStorageProvider eventStorageProvider) {
-            _eventModelStorage = eventStorageProvider.GetNeuEventModelStorage();
-        }
-
-        public async Task DoSomehingAsync() {
-            await _eventModelStorage.InsertAsync(new NewNeuEventModel());
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-    [SQLite.Table("courses")]
     public class EventModel
     {
         [PrimaryKey, AutoIncrement]
---
+++ NeuToDo/Scratch/Scratch.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using SQLite;
+
+namespace NeuToDo.Scratch
+{
+    /// <summary>
+    /// Just Scratch
+    /// </summary>
+    public class NewEventModel
+    {
+        [PrimaryKey, AutoIncrement] public int Id { get; set; }
+    }
+
+    public class NewNeuEventModel : NewEventModel
+    {
+    }
+
+    public interface INewEventModelStorage<T> where T : NewEventModel
+    {
+        Task InsertAsync(T t);
+    }
+
+    public class NewEventModelStorage<T> : INewEventModelStorage<T>
+        where T : NewEventModel
+    {
+        private SQLiteAsyncConnection _connection;
+
+        public async Task InsertAsync(T t)
+        {
+            await _connection.InsertAsync(t);
+        }
+    }
+
+    public interface IEventStorageProvider
+    {
+        INewEventModelStorage<NewNeuEventModel> GetNeuEventModelStorage();
+    }
+
+    public class EventStorageProvider : IEventStorageProvider
+    {
+        public INewEventModelStorage<NewNeuEventModel> GetNeuEventModelStorage()
+        {
+            return new NewEventModelStorage<NewNeuEventModel>();
+        }
+    }
+
+    public class NeuLogin
+    {
+        private INewEventModelStorage<NewNeuEventModel> _eventModelStorage;
+
+        public NeuLogin(IEventStorageProvider eventStorageProvider)
+        {
+            _eventModelStorage = eventStorageProvider.GetNeuEventModelStorage();
+        }
+
+        public async Task DoSomehingAsync()
+        {
+            await _eventModelStorage.InsertAsync(new NewNeuEventModel());
+        }
+    }
+}
