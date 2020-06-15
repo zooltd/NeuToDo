1d32e0502f9a658ad61c8ea556810d1b4483ac18
Zhiping Qiu <qiuzhiping20102@163.com>
2020-06-16 00:15:30 +0800

    IHttpClientFactory Config

---
+++ NeuToDo/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NeuToDo.Services {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddHttpClient("mooc", config => {
+                config.BaseAddress = new Uri("https://www.icourse163.org/");
+                config.DefaultRequestHeaders.Add("User-Agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36");
+            }).ConfigurePrimaryHttpMessageHandler((() => new HttpClientHandler() {
+                AllowAutoRedirect = true,
+                UseCookies = true,
+                CookieContainer = new CookieContainer()
+            }));
+        }
+    }
+}
