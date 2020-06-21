15a73d721bea674b7613f0be03c29952d5ccc7b1
Youhan Wu <youhan.wu@outlook.com>
2020-06-21 21:38:25 +0800

    新增图片

---
+++ NeuToDo/Converters/TypeNameConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace NeuToDo.Converters
+{
+    public class TypeNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value.GetType().Name;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
