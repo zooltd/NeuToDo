223ed320e383c638ebfb4e1e5d59b2939f443bf8
Youhan Wu <youhan.wu@outlook.com>
2020-07-03 01:38:12 +0800

    temp

---
+++ NeuToDo.UnitTest/ViewModels/EventDetailViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using NeuToDo.Services;
+using NeuToDo.Utils;
+using NeuToDo.ViewModels;
+
+namespace NeuToDo.UnitTest.ViewModels
+{
+    public class EventDetailViewModelTest
+    {
+        public void Init()
+        {
+            var eventStorageMock = new Mock<IEventModelStorageProvider>();
+            var mockEventStorage = eventStorageMock.Object;
+            var popupNavigationServiceMock = new Mock<IPopupNavigationService>();
+            var mockPopupNavigationService = popupNavigationServiceMock.Object;
+            var academicCalendarMock = new Mock<IAcademicCalendar>();
+            var mockAcademicCalendar = academicCalendarMock.Object;
+            var alertServiceMock = new Mock<IAlertService>();
+            var mockAlertService = alertServiceMock.Object;
+            var eventDetailViewModel = new EventDetailViewModel(mockEventStorage, mockPopupNavigationService,
+                mockAcademicCalendar, mockAlertService);
+        }
+    }
+}
