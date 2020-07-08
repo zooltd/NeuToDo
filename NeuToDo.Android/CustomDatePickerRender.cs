45aa21dc974596f45bfbd32785a61eb67b26a9cb
Youhan Wu <youhan.wu@outlook.com>
2020-07-08 18:43:18 +0800

    修改MoocEventDetaiPage样式

---
+++ NeuToDo.Android/CustomDatePickerRender.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using NeuToDo.Components;
+using NeuToDo.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerRender))]
+
+namespace NeuToDo.Droid
+{
+    public class CustomDatePickerRender : DatePickerRenderer
+    {
+        public CustomDatePickerRender(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
