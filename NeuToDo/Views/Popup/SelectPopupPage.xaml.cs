db695d18dfa99ed9439842d09910f6aeab874ecb
Youhan Wu <youhan.wu@outlook.com>
2020-07-17 23:01:30 +0800

    fix select mooc courses page

--- NeuToDo/Views/Popup/SelectPopupPage.xaml.cs
+++ NeuToDo/Views/Popup/SelectPopupPage.xaml.cs
@@ -14,6 +14,23 @@ namespace NeuToDo.Views.Popup
             InitializeComponent();
         }
 
+        protected override void OnAppearingAnimationBegin()
+        {
+            base.OnAppearingAnimationBegin();
+
+            FrameContainer.HeightRequest = 400;
+            FrameContainer.WidthRequest = 400;
+
+            if (IsAnimationEnabled) return;
+
+            SaveButton.Scale = 1;
+            SaveButton.Opacity = 1;
+
+            CollectionView.TranslationX = 0;
+            CollectionView.Opacity = 1;
+        }
+
+
         protected override bool OnBackgroundClicked()
         {
             CloseAllPopup();
